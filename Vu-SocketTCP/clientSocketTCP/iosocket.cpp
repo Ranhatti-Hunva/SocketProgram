133a1d44ef34b886210c6f6ae33e9c199b950bde
Ranhatti-Hunva <34017271+Ranhatti-Hunva@users.noreply.github.com>
2018-09-07 17:44:32 +0700

    read file

--- Vu-SocketTCP/clientSocketTCP/iosocket.cpp
+++ Vu-SocketTCP/clientSocketTCP/iosocket.cpp
@@ -1,6 +1,4 @@
 #include "iosocket.h"
-#include "tcphelper.h"
-//#include "udphelper.h"
 
 // using to analyser user command.
 void splits_string(const std::string& subject, std::vector<std::string>& container)
@@ -37,52 +35,65 @@ void read_terminal(bool& end_connection, TCPclient& client_helper, msg_queue& ms
             {
                 std::string user_cmd_str;
                 getline(std::cin, user_cmd_str);
-                if(!user_cmd_str.empty())
-                {
 
-                    if(user_cmd_str.compare("#"))
-                    {                        
+                if(!user_cmd_str.empty())
+                {                    
+                    if(!user_cmd_str.compare("#"))
+                    {
+                        end_connection = true;
+                        is_error = false;
+                        break;
+                    }
+                    else
+                    {
                         // Packet msg and push to msg_send_queue
                         std::vector<std::string> container;
                         splits_string(user_cmd_str, container);
 
                         if(container.size() < 2)
                         {
-                            // Test 1000 msg/s.
-                            if (!user_cmd_str.compare("1000"))
-                            {
-                                for (int i=0; i<1000; i++)
-                                {
-                                    msg_text msg_send;
-                                    msg_send.msg = "all/hello";
-                                    msg_send.type_msg = MSG;
-
-                                    std::vector<unsigned char> element;
-                                    client_helper.packed_msg(msg_send, element);
-                                    msg_wts.push(element, Q_MSG);
-                                }
-                            }
-                            else
-                            {
-                                printf("=> Wrong format message or no massge to send !! \n");
-                            };
+                            printf("=> Wrong format message or no massge to send !! \n");
                         }
                         else
                         {
-                            msg_text msg_send;
-                            msg_send.msg = user_cmd_str;
-                            msg_send.type_msg = MSG;
+                            if (!container[0].compare("file"))
+                            {
+                                // Send from file
+                                vector<file_infor> new_files;
 
-                            std::vector<unsigned char> element;
-                            client_helper.packed_msg(msg_send, element);
-                            msg_wts.push(element, Q_MSG);
+                                new_files = getDirectoryFiles("../build-clientSocketTCP-Desktop_Qt_5_11_1_clang_64bit-Debug/filemsg", "txt");
+
+                                if(files.empty())
+                                {
+                                    for (unsigned long i=0; i< new_files.size(); i++)
+                                    {
+                                        push_file(new_files[i], client_helper, msg_wts, container[1]);
+                                    };
+                                }
+                                else
+                                {
+                                    for (unsigned long i=0; i< new_files.size(); i++)
+                                    {
+                                        if(is_wts(files, new_files[i]))
+                                        {
+                                            push_file(new_files[i], client_helper, msg_wts, container[1]);
+                                        };
+                                    };
+                                };
+
+                                files = new_files;
+                            }
+                            else
+                            {
+                                msg_text msg_send;
+                                msg_send.msg = user_cmd_str;
+                                msg_send.type_msg = MSG;
+
+                                std::vector<unsigned char> element;
+                                client_helper.packed_msg(msg_send, element);
+                                msg_wts.push(element, Q_MSG);
+                            };
                         };
-                    }
-                    else
-                    {
-                        end_connection = true;
-                        is_error = false;
-                        break;
                     };
                 };
             };
@@ -115,6 +126,107 @@ bool is_reconnect(int& client_fd)
         }
     };
 };
+
+std::vector<file_infor> getDirectoryFiles(const std::string& dir, const char* ext) {
+  std::vector<file_infor> files;
+  std::shared_ptr<DIR> directory_ptr(opendir(dir.c_str()), [](DIR* dir){ dir && closedir(dir); });
+  struct dirent *dirent_ptr;
+  if (!directory_ptr) {
+    std::cout << "Error opening : " << std::strerror(errno) << dir << std::endl;
+    return files;
+  }
+
+  char *p;
+
+  while ((dirent_ptr = readdir(directory_ptr.get())) != nullptr) {
+
+      size_t len = std::string(dirent_ptr->d_name).length()+ 1;
+      char* s = new char[ len ];
+      memset(s, 0, len*sizeof(char));
+      memcpy(s, std::string(dirent_ptr->d_name).c_str(), (len - 1)*sizeof(char));
+
+      p=strtok(dirent_ptr->d_name,".");
+
+      if (p != nullptr)
+      {
+          p = strtok(nullptr,".");
+          if(p != nullptr)
+          {
+              if(!strcmp(p,ext))
+              {
+                 file_infor file;
+                 file.name = dir + "/" + std::string(dirent_ptr->d_name)+".txt";
+
+                 std::string path = file.name;
+                 stat(path.c_str(),&file.attr);
+
+                 files.push_back(file);
+              };
+          };
+      };
+
+  }
+  return files;
+};
+
+bool is_wts(vector<file_infor> files_list, file_infor file)
+{
+    for(unsigned long i = 0; i<files_list.size(); i++)
+    {
+        if(files_list[i].name ==  file.name)
+        {
+            if(file.attr.st_mtime != files_list[i].attr.st_mtime)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            };
+        };
+    };
+    return true;
+};
+
+bool push_file(file_infor file, TCPclient& client_helper, msg_queue& msg_wts, string user_forward)
+{
+    int fd;
+    off_t /*size_of_file,*/ bytes_readed;
+//    struct stat file_infor;
+    const int bufsize = 1024;
+    char buffer[bufsize] = {0};
+
+    if ((fd = open(file.name.c_str(),O_RDONLY))==-1)
+    {
+        perror("=>open fail");
+        return false;
+    };
+
+//    if (stat(file.name.c_str(),&file_infor)==-1)
+//    {
+//        perror("stat fail");
+//        return false;
+//    }
+//    else
+//        size_of_file=file_infor.st_size;
+
+    bytes_readed = read(fd,buffer,bufsize);
+
+    while(bytes_readed)
+    {
+        msg_text msg_send;
+        msg_send.msg = user_forward + "/" + string(buffer);
+        msg_send.type_msg = MSG;
+
+        std::vector<unsigned char> element;
+        client_helper.packed_msg(msg_send, element);
+        msg_wts.push(element, Q_MSG);
+
+        bytes_readed = read(fd,buffer,bufsize);
+    };
+    return true;
+};
+
 
 
 
--- Vu-SocketTCP/clientSocketTCP/iosocket.h
+++ Vu-SocketTCP/clientSocketTCP/iosocket.h
@@ -2,40 +2,37 @@
 #define IOSOCKET_H
 
 #include <iostream>
-#include <unistd.h>
-#include <stdio.h>
-#include <sys/socket.h>
-#include <stdlib.h>
-#include <netinet/in.h>
-#include <netdb.h>
 #include <string.h>
-#include <arpa/inet.h>
-#include <sys/types.h>
-#include <errno.h>
-#include <arpa/inet.h>
-#include <thread>
-#include <mutex>
 #include <vector>
 #include <fcntl.h>
-
-// Time function, sockets, htons... file stat
-#include <sys/time.h>
-#include <sys/uio.h>
+#include <dirent.h>
+#include <cstring>
 #include <sys/stat.h>
-
-// File function and bzero
-#include <strings.h>
-#include <future>
+#include <sys/types.h>
 
 #include "msgqueue.h"
 #include "tcphelper.h"
 
 extern std::string user_name;
 
+struct file_infor
+{
+    std::string name;
+    struct stat attr;
+};
+
+static vector<file_infor> files;
+
 void splits_string(const std::string& subject, std::vector<std::string>& container);
 
 void read_terminal(bool& end_connection, TCPclient& client_helper, msg_queue& msg_wts);
 
 bool is_reconnect(int& client_fd);
+
+std::vector<file_infor> getDirectoryFiles(const std::string& dir, const char* ext);
+
+bool is_wts(vector<file_infor> files_list, file_infor file);
+
+bool push_file(file_infor file, TCPclient& client_helper, msg_queue& msg_wts, string user_forward);
 
 #endif // IOSOCKET_H
